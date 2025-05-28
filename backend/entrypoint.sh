c379e7f914672f143861ad489912f5e190e06304
jhonatan grajales <jagrajales@grupodecor.com>
2025-05-28 08:00:31 -0500

    Se crea script para diagnositicar porque no encuentra el Container del backedn

---
+++ backend/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Este script se ejecutará cuando el contenedor se inicie.
+
+echo "Verificando que gunicorn_config.py exista..."
+if [ -f "/app/gunicorn_config.py" ]; then
+    echo "gunicorn_config.py encontrado."
+else
+    echo "ERROR: gunicorn_config.py NO ENCONTRADO."
+    exit 1
+fi
+
+echo "Iniciando Gunicorn..."
+# Ejecuta Gunicorn, pasándole la configuración y la aplicación WSGI.
+# Usamos exec para que Gunicorn reemplace el proceso del script, permitiendo a Podman manejar señales.
+exec gunicorn --config gunicorn_config.py Preguntas_frecuentes.wsgi:application
