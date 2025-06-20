ffbb2e835ec6233865f73da66a1647ae49981e31
jhonatan grajales <jagrajales@grupodecor.com>
2025-06-20 11:47:43 -0500

    Ignora advertencia de tipado en queryset de Tip y Manual en views.py

--- backend/recursos/views.py
+++ backend/recursos/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Tip, Manual
+from .serializers import TipSerializer, ManualSerializer
 
 # Create your views here.
+
+class TipViewSet(viewsets.ModelViewSet):
+    queryset = Tip.objects.all()  # type: ignore
+    serializer_class = TipSerializer
+
+class ManualViewSet(viewsets.ModelViewSet): 
+    queryset = Manual.objects.all() # type: ignore
+    serializer_class = ManualSerializer
