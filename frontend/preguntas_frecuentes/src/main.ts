1abe39c4768f0516caf9f163258940366f4d61e0
jhonatan grajales <jagrajales@grupodecor.com>
2025-06-25 22:10:42 -0500

    feat: se agrega libreria para manejar notificaciones

--- frontend/preguntas_frecuentes/src/main.ts
+++ frontend/preguntas_frecuentes/src/main.ts
@@ -1,6 +1,8 @@
 import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -12,5 +14,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Toast)
 
 app.mount('#app')
