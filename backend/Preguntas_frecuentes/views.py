2f3a91ee99608b720f75ab95081c2a926d50c021
jhonatan grajales <jagrajales@grupodecor.com>
2025-06-26 23:42:56 -0500

    Confiuracion de las views para restaurar el login

--- backend/Preguntas_frecuentes/views.py
+++ backend/Preguntas_frecuentes/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from .serializers import LoginSerializer # Importa el nuevo serializer
 import logging
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Obtener una instancia del logger
 logger = logging.getLogger(__name__)
@@ -16,26 +17,29 @@ def home(request):
 
 class LoginAPIView(APIView):
     """
-    API para el inicio de sesión de usuarios.
-    Recibe username y password, y devuelve un token de autenticación.
+    API para el inicio de sesión de usuarios usando Directorio Activo y JWT.
     """
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
-            username = serializer.validated_data['username']  # type: ignore
-            password = serializer.validated_data['password']  # type: ignore
-            
+            validated = serializer.validated_data or {}
+            username = validated.get('username')  # type: ignore
+            password = validated.get('password')  # type: ignore
             user = authenticate(request, username=username, password=password)
-            
             if user is not None:
-                # El usuario es autenticado, obtener o crear el token
-                token, created = Token.objects.get_or_create(user=user)  # type: ignore
-                return Response({'token': token.key}, status=status.HTTP_200_OK)
+                if user.is_active:
+                    refresh = RefreshToken.for_user(user)
+                    return Response({
+                        'access': str(refresh.access_token),
+                        'refresh': str(refresh),
+                        'username': user.username
+                    }, status=status.HTTP_200_OK)
+                else:
+                    logger.warning(f"Usuario inactivo o bloqueado: {username}")
+                    return Response({'detail': 'La cuenta está inactiva o bloqueada.'}, status=status.HTTP_403_FORBIDDEN)
             else:
-                # Autenticación fallida
                 logger.warning(f"Intento de login fallido para el usuario: {username}")
-                return Response({'error': 'Credenciales inválidas'}, status=status.HTTP_401_UNAUTHORIZED)
+                return Response({'detail': 'Credenciales inválidas o usuario no encontrado en AD.'}, status=status.HTTP_401_UNAUTHORIZED)
         else:
-            # Datos de entrada inválidos
             logger.error(f"Error de validación en el login: {serializer.errors}")
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
