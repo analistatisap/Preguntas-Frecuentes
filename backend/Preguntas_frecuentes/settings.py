acb7b5da10aa213b9c33b3da943ab9171eb5b8d6
jhonatan grajales <jagrajales@grupodecor.com>
2025-08-14 11:53:21 -0500

    se modifica archivo config

--- backend/Preguntas_frecuentes/settings.py
+++ backend/Preguntas_frecuentes/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR.parent / 'staticfiles'
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://preguntame.grupodecor.com:5046/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
